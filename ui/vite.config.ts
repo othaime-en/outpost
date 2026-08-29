8e2898e90a28e9a70925aa8262680a40b1fefb46
othaime-en <61498231+othaime-en@users.noreply.github.com>
2026-08-29 13:03:46 +0300

    chore: allow all hosts for cloudflare tunneling

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -6,6 +6,8 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 3000,
+        // allow all host for cloudflare tunnelling
+        allowedHosts: true,
         // Proxy API calls from the dev server to FastAPI.
         // This means the React app can call `/api/environments` in development
         // and Vite will forward it to http://localhost:8000/environments —
